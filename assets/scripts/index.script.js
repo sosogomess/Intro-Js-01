56354470143292b8f899e0716c4d56af4cc376a4
sophia gomes <sophiagomes.tds2@gmail.com>
2025-02-04 08:34:47 -0300

    feat: create first JS function with HTML

--- assets/scripts/index.script.js
+++ assets/scripts/index.script.js
@@ -1,5 +1,7 @@
 function exibirNome() {
   let nome = document.getElementById("nome").value;
 
-  console.log("Nome digitado: ", nome);
+  //  console.log("Nome digitado: ", nome);
+
+  document.getElementById("msg").innerHTML = nome;
 }
